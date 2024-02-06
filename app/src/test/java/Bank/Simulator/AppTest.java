84c1b7294a5a2069bcc1066bc1d4bde2d48a102b
Paul <paulhades225@gmail.com>
2024-02-06 20:42:57 +0100

    spec

--- app/src/test/java/Bank/Simulator/AppTest.java
+++ app/src/test/java/Bank/Simulator/AppTest.java
@@ -7,11 +7,8 @@ import org.junit.Test;
 
 import Banksim.App;
 
-import static org.junit.Assert.*;
-
 public class AppTest {
     @Test public void appHasAGreeting() {
         App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
 }
